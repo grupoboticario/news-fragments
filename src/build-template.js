d1058e33585f594c3fffb4e9082e3e074570ad83
Matheus Barão Fiorin <matheusfiiorin@gmail.com>
2020-02-20 16:34:21 -0300

    Create base template compile function

---
+++ src/build-template.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const Handlebars = require("handlebars");
+
+module.exports.renderTemplate = function(changelogTemplate, data) {
+  let compiledTemplate = Handlebars.compile(changelogTemplate);
+  return compiledTemplate(data);
+};
+
+module.exports.saveChangelogToFile = function(filePath, renderedTemplate) {
+  const fileDescriptor = fs.openSync(filePath, "a+");
+
+  const oldData = fs.readFileSync(filePath);
+  const newData = new Buffer.from(renderedTemplate);
+
+  fs.writeSync(fileDescriptor, newData, 0, newData.length, 0);
+  fs.writeSync(fileDescriptor, oldData, 0, oldData.length, newData.length);
+
+  fs.closeSync(fileDescriptor);
+};

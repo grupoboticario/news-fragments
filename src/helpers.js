a0f5ea5ed6c7b9d2da32bbd3ec1dd84b5a258fed
Matheus Barão Fiorin <matheusfiiorin@gmail.com>
2020-03-02 09:14:31 -0300

    Create helper functions to check file/folder existence

---
+++ src/helpers.js
@@ -0,0 +1,15 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+module.exports.checkChangelogFile = function(changelogFile) {
+  if (!fs.existsSync(changelogFile)) {
+    fs.writeFileSync(changelogFile, "");
+  }
+};
+
+module.exports.checkFragmentsFolder = function(fragmentsFolder) {
+  const fragmentsPath = path.join(fragmentsFolder, ".gitkeep");
+  if (!fs.existsSync(fragmentsPath)) {
+    fs.outputFileSync(fragmentsPath, "");
+  }
+};

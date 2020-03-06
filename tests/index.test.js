16432821d776509d44efc054d49f36a064b61ed2
Matheus Barão Fiorin <matheusfiiorin@gmail.com>
2020-03-06 15:15:58 -0300

    Create new tests to index

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,16 +1,23 @@
 const { factory, runTasks } = require("release-it/test/util");
+const fs = require("fs-extra");
 const mockFs = require("mock-fs");
+const moment = require("moment");
 const Plugin = require("../src/index");
 const pjson = require("../package.json");
 const semver = require("semver");
 
 const namespace = "newsfragments";
+let newsFragments;
 
 beforeEach(() => {
   mockFs({
-    features: {},
+    fragments: {
+      ".gitkeep": "",
+      "collect-me.feature": "Coleta com sucesso"
+    },
     "CHANGELOG.md": ""
   });
+  newsFragments = new Plugin();
 });
 
 afterEach(() => {
@@ -22,7 +29,34 @@ test("should not throw errors and do a patch increase", async () => {
   const plugin = factory(Plugin, { namespace, options });
   expect(await runTasks(plugin)).toStrictEqual({
     latestVersion: pjson.version,
-    name: "@grupoboticario/newsfragments",
+    name: "@grupoboticario/news-fragments",
     version: semver.inc(pjson.version, "patch")
   });
+});
+
+test("should collect a fragment when running init method", () => {
+  newsFragments.init();
+  expect(newsFragments.fragmentsToBurn).toStrictEqual([
+    { title: "Features", fragmentEntries: ["Coleta com sucesso"] }
+  ]);
+  expect(newsFragments.fragmentsToDelete).toStrictEqual([
+    "fragments/collect-me.feature"
+  ]);
+});
+
+test("should delete fragments when generated changelog", () => {
+  const version = pjson.version;
+  const date = moment().format("YYYY-MM-DD");
+  const expectedOutput = `# [${version}] - (${date})
+## Features
+* Coleta com sucesso
+`;
+  newsFragments.init();
+  newsFragments.afterRelease();
+  expect(
+    fs.readdirSync(newsFragments.baseConfig.fragmentsFolder)
+  ).toStrictEqual([".gitkeep"]);
+  expect(
+    fs.readFileSync(newsFragments.baseConfig.changelogFile, "utf8")
+  ).toStrictEqual(expectedOutput);
 });

1936aa59d9fe4ddd134903b2137532e139685d02
matheusfiorin <matheusfiiorin@gmail.com>
2020-12-02 17:44:23 -0300

    Add burn information in cli/index.js

--- src/cli/index.js
+++ src/cli/index.js
@@ -5,24 +5,28 @@ const meow = require("meow");
 const chalk = require("chalk");
 const { create } = require("./create");
 const { preview } = require("./preview");
+const { burn } = require("./burn");
 
 const cli = meow(
   `
 	Usage
     $ news-fragments create <fragment-type> <fragment-text>
     $ news-fragments preview
+    $ news-fragments burn <version>
 
   Options
     --help
 
 	Examples
     $ news-fragments create feature "New feature"
+    $ news-fragments burn 0.0.1
 `
 );
 
 const commands = {
   create: create,
   preview: preview,
+  burn: burn,
 };
 
 const command = commands[cli.input[0]];

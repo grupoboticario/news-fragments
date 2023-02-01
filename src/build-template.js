d3a130004048db10e6a7b4a48ab588574a7c855d
Rayane S <37119820+RayaneSlimani@users.noreply.github.com>
2023-02-01 12:16:11 +0100

    Update build-template.js

--- src/build-template.js
+++ src/build-template.js
@@ -18,16 +18,25 @@ export const renderTemplate = function (changelogTemplate, data, version) {
 };
 
 export const saveChangelogToFile = function (filePath, renderedTemplate) {
-  const fileDescriptor = fs.openSync(filePath, "a+");
+  // const fileDescriptor = fs.openSync(filePath, "a+");
+  if (!fs.existsSync(filePath)) {
+    // asynchronously create a directory
+    fs.writeFileSync(filePath, renderedTemplate)
+    return
+  }
 
   const oldData = fs.readFileSync(filePath);
   const newData = new Buffer.from(renderedTemplate);
 
-  fs.writeSync(fileDescriptor, newData, 0, newData.length, 0);
-  fs.writeSync(fileDescriptor, oldData, 0, oldData.length, newData.length);
+  // fs.writeSync(fileDescriptor, newData, 0, newData.length, 0);
+  // fs.writeSync(fileDescriptor, oldData, 0, oldData.length, newData.length);
 
-  fs.closeSync(fileDescriptor);
+  // fs.closeSync(fileDescriptor);
+  // Check if the file exists
+  fs.unlinkSync(filePath);
+  fs.appendFileSync(filePath, newData + oldData);
 };
+
 
 export const generateTemplateData = function (
   newVersion,

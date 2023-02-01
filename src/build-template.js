38de75fb2dc927ebc2dc91c6db4cc6cbb47e7091
Rayane S <37119820+RayaneSlimani@users.noreply.github.com>
2023-02-01 12:25:08 +0100

    Update build-template.js
    lint change.

--- src/build-template.js
+++ src/build-template.js
@@ -19,20 +19,16 @@ export const renderTemplate = function (changelogTemplate, data, version) {
 
 export const saveChangelogToFile = function (filePath, renderedTemplate) {
   // const fileDescriptor = fs.openSync(filePath, "a+");
+  // Check if the file exists
   if (!fs.existsSync(filePath)) {
     // asynchronously create a directory
-    fs.writeFileSync(filePath, renderedTemplate)
-    return
-  }
+    fs.writeFileSync(filePath, renderedTemplate);
+    return;
+  };
 
   const oldData = fs.readFileSync(filePath);
   const newData = new Buffer.from(renderedTemplate);
-
-  // fs.writeSync(fileDescriptor, newData, 0, newData.length, 0);
-  // fs.writeSync(fileDescriptor, oldData, 0, oldData.length, newData.length);
-
-  // fs.closeSync(fileDescriptor);
-  // Check if the file exists
+  
   fs.unlinkSync(filePath);
   fs.appendFileSync(filePath, newData + oldData);
 };

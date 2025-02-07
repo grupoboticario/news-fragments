bf055833cfa40fa70e2db4d204c8aedfadadc69c
Jair Henrique <jair.henrique@gmail.com>
2025-02-07 13:59:53 -0300

    Update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("plugin:prettier/recommended"), {
+    plugins: {
+        "simple-import-sort": simpleImportSort,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            Atomics: "readonly",
+            SharedArrayBuffer: "readonly",
+        },
+
+        ecmaVersion: 2020,
+        sourceType: "module",
+    },
+
+    rules: {
+        "simple-import-sort/imports": "error",
+    },
+}];
